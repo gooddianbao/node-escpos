8d235baef0cc0a73890805df6cfc2b733849e3ab
Roger Huang <roger@beautimode.com>
2020-07-31 11:33:57 +0800

    * Remove dependency to Bluetooth.

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ exports.USB     = require('./adapter/usb');
 exports.Serial  = require('./adapter/serial');
 exports.Network = require('./adapter/network');
 exports.Console = require('./adapter/console');
-exports.Bluetooth = require('./adapter/bluetooth');
-exports.RawBT   = require('./adapter/rawbt');
 
 /**
  * Printer Supports
